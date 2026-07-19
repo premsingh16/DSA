7cb3f6ce8919a36e981415a3b66c45e337c68669
premsingh16 <premsinghchauhan06531@gmail.com>
2026-07-19 23:47:02 +0530

    Time: 0 ms (100%), Space: 9.6 MB (8.14%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>freq;
+        for(char ch : s) freq[ch]++;
+        for(char ch : t) freq[ch]--;
+        for(auto it : freq){
+            if(it.second < 0) return it.first;
+        }
+        return ' ';
+    }
+};
