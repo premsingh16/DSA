92c6266404947feb7a45244c3201601f41c5a812
premsingh16 <premsinghchauhan06531@gmail.com>
2026-08-18 15:26:34 +0530

    Time: 28 ms (48.47%), Space: 24.2 MB (21.61%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    void bfs(int row , int col , vector<vector<int>>&vis, vector<vector<char>>& grid){
+        vis[row][col] = 1;
+        queue<pair<int,int>>q;
+        q.push({row, col});
+        int n = grid.size();
+        int m = grid[0].size();
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            // traverse in the neighbour 
+            for(int delrow = -1; delrow <= 1; delrow++){
+                for(int delcol = -1; delcol <= 1 ; delcol++){
+                    // Skip diagonals and current cell
+                    if(abs(delrow) + abs(delcol) != 1)
+                        continue;
+                    int neighrow = row + delrow;
+                    int neighcol = col + delcol;
+                    if(neighrow >= 0 && neighrow < n && neighcol >= 0 && neighcol < m 
+                    && grid[neighrow][neighcol] == '1' && !vis[neighrow][neighcol]){
+                        vis[neighrow][neighcol] = 1;
+                        q.push({neighrow,neighcol});
+                    }
+                }
+            }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        vector<vector<int>>vis(n , vector<int>(m , 0));
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col<m; col++){
+                if(!vis[row][col] && grid[row][col] == '1'){
+                    cnt++;
+                    bfs(row , col , vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
