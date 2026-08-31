96ab22743ee99594e1501eb7ffe377a1d80f063e
premsingh16 <premsinghchauhan06531@gmail.com>
2026-08-31 11:49:32 +0530

    Time: 3 ms (36.52%), Space: 92.1 MB (58.71%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -4,57 +4,29 @@ public:
         int n = nums.size();
         int minEl = INT_MAX;
         int maxEl = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < minEl)
+        int minIndex = -1;
+        int maxIndex = -1;
+        // Find min, max and their indices
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < minEl) {
                 minEl = nums[i];
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > maxEl)
+                minIndex = i;
+            }
+            if (nums[i] > maxEl) {
                 maxEl = nums[i];
+                maxIndex = i;
+            }
         }
+        // Case 1: Both removed from the front
+        int front = max(minIndex, maxIndex) + 1;
 
-        // From front
-        int cnt = 0;
-        int length = 0;
-        for (int i = 0; i < n; i++) {
-            length++;
+        // Case 2: Both removed from the back
+        int back = n - min(minIndex, maxIndex);
 
-            if (nums[i] == maxEl || nums[i] == minEl)
-                cnt++;
+        // Case 3: One from front, one from back
+        int mixed = min(minIndex, maxIndex) + 1
+                  + n - max(minIndex, maxIndex);
 
-            if (cnt == 2)
-                break;
-        }
-
-        // From back
-        int cnt2 = 0;
-        int length2 = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            length2++;
-
-            if (nums[i] == maxEl || nums[i] == minEl)
-                cnt2++;
-
-            if (cnt2 == 2)
-                break;
-        }
-
-        // One from front, one from back
-        int len1 = 0;
-        for (int i = 0; i < n; i++) {
-            len1++;
-            if (nums[i] == maxEl || nums[i] == minEl) break;   
-        }
-
-        int len2 = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            len2++;
-            if (nums[i] == maxEl || nums[i] == minEl) break;   
-        }
-        // +2 because the min/max elements themselves
-        // also need to be deleted
-        int length3 = len1 + len2 ;
-        return min({length, length2, length3});
+        return min({front, back, mixed});
     }
 };
